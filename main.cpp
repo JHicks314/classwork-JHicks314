e9783c74e051de30db2bcece6d8d0a52e8d80756
JonMarc Hicks <jonmarchicks@JonMarcs-MBP.MissouriState.edu>
2022-02-18 13:07:17 -0600

    created fib function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 
+//Recursive function: calculate the nth fibonacci number
+/**
+ *@brief Calculate the nth fib number
+ *
+ *@param n designates which number in the sequense we desire.
+ *@return int the nth number in the fibonacci sequence.
+ */
+int fib(int);
+
+
 int main ( )
 {
-    std::cout << "Hello, World!" << std::endl;
+    for (int i{0}; i < 6; ++i){
+    std::cout << "fib(" << i << ") = " << fib(i) << std::endl;
+    }
     return 0;
+}
+
+int fib(int n){
+    if (n == 0 or n == 1){
+        //base case
+        return n;
+    }
+    return fib(n-1) + fib(n-2);
 }
